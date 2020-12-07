15bb58e250bee0afd7b32199ae7b56317d24b71b
andryue <kacperwojak17@gmail.com>
2020-12-07 15:23:56 +0100

    create basics of main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:outline_material_icons/outline_material_icons.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,39 +10,71 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Memes App',
-      theme: ThemeData(
-        textTheme: ThemeData.light().textTheme.copyWith(
-          headline1: TextStyle(fontSize: 40, fontWeight: FontWeight.w700, color: Colors.white),
-          headline2: TextStyle(fontSize: 30, fontWeight: FontWeight.w700, color: Colors.white),
-          headline3: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.white),
+        debugShowCheckedModeBanner: false,
+        title: 'Memes App',
+        theme: ThemeData(
+          textTheme: ThemeData.light().textTheme.copyWith(
+                headline1: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+                headline2: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+                headline3: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+          backgroundColor: Color.fromARGB(255, 43, 39, 51),
+          fontFamily: 'Lato',
+          primaryColor: Color.fromARGB(255, 25, 23, 32),
+          accentColor: Color.fromARGB(255, 43, 39, 51),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        backgroundColor: Color.fromARGB(255,43, 39, 51),
-        fontFamily: 'Lato',
-        primaryColor: Color.fromARGB(255,25, 23, 32),
-        accentColor: Color.fromARGB(255,43, 39, 51),
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
-    );
+        initialRoute: '/',
+        routes: {'/': (context) => HomePage()});
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(5.0),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 23, 19, 31).withOpacity(0.5) ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: Icon(OMIcons.image,color: Colors.white,),
+              onPressed: () {},
+              iconSize: 40.0,
+            ),IconButton(
+              icon: Icon(OMIcons.image,color: Colors.white),
+              onPressed: () {},
+              iconSize: 40.0,
+            ),IconButton(
+              icon: Icon(OMIcons.image,color: Colors.white),
+              onPressed: () {},
+              iconSize: 40.0,
+            )
+          ],
+        ),
+      ),
       backgroundColor: Theme.of(context).backgroundColor,
-      appBar: AppBar(),
-      body: Text("XD",style: Theme.of(context).textTheme.headline1,),
+      body: Text(
+        "XD",
+        style: Theme.of(context).textTheme.headline1,
+      ),
     );
   }
 }
