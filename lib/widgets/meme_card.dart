9f5367116ee349868fd19b9f80cc438389394965
andryue <kacperwojak17@gmail.com>
2020-12-08 10:26:52 +0100

    finish screen

--- lib/widgets/meme_card.dart
+++ lib/widgets/meme_card.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
 
 class MemeCard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top - MediaQuery.of(context).padding.bottom;
+    final screenHeight = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).padding.top -
+        MediaQuery.of(context).padding.bottom;
     return Container(
       margin: EdgeInsets.symmetric(vertical: 20.0),
-      height: screenHeight/2.2 ,
+      height: screenHeight / 2.2,
       width: double.infinity,
       child: Stack(
         children: [
@@ -19,20 +20,26 @@ class MemeCard extends StatelessWidget {
               'https://thepsychologist.bps.org.uk/sites/thepsychologist.bps.org.uk/files/img_9685.jpg',
               fit: BoxFit.fill,
               width: double.infinity,
-              height:  screenHeight/2.5,
+              height: screenHeight / 2.5,
             ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
+              padding: EdgeInsets.all(20.0),
               width: 70,
               height: 70,
               decoration: BoxDecoration(
                 color: Theme.of(context).primaryColor.withOpacity(1),
                 borderRadius: BorderRadius.circular(100.0),
               ),
-              child: Icon(
-               OMIcons.favoriteBorder,size: 32.0,color: Colors.white,),
+              child: FittedBox(
+                child: Icon(
+                  OMIcons.favoriteBorder,
+                  size: 32.0,
+                  color: Colors.white,
+                ),
+              ),
             ),
           )
         ],
