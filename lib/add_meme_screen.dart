cc047a47108fccf9dc53c7cf7b383f0d02801f62
andryue <kacperwojak17@gmail.com>
2020-12-08 16:18:09 +0100

    add text style buttons

--- lib/add_meme_screen.dart
+++ lib/add_meme_screen.dart
@@ -11,112 +11,166 @@ class AddMemeScreen extends StatefulWidget {
 class _AddMemeScreenState extends State<AddMemeScreen> {
   MemeText t1 = MemeText();
   MemeText t2 = MemeText();
+
   @override
   Widget build(BuildContext context) {
-    return Form(
+    return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: double.infinity,
-              height: 300,
-              child: Stack(
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: Stack(
+                  children: [
+                    Image.network(
+                      'https://bingoland.pl/userdata/public/gfx/2652/tlo-fotograficzne-biale.jpg',
+                      width: double.infinity,
+                      fit: BoxFit.fill,
+                      height: 300,
+                    ),
+                    Align(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        child: AutoSizeText(
+                          t1.text,
+                          textAlign: TextAlign.center,
+                          maxLines: 3,
+                          style:
+                              TextStyle(color: Colors.grey[900], fontSize: 36),
+                        ),
+                      ),
+                      alignment: Alignment.topCenter,
+                    ),
+                    Align(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 15, vertical: 10),
+                        child: AutoSizeText(
+                          t2.text,
+                          textAlign: TextAlign.center,
+                          maxLines: 3,
+                          style:
+                              TextStyle(color: Colors.grey[900], fontSize: 36),
+                        ),
+                      ),
+                      alignment: Alignment.bottomCenter,
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                    hintText: 'Enter Top Text',
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.grey),
+                    focusedBorder: border,
+                    enabledBorder: border.copyWith(
+                        borderSide: BorderSide(color: Colors.grey[600]))),
+                style: Theme.of(context).textTheme.headline6,
+                onChanged: (val) => setState(() => t1.text = val),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                cursorColor: Colors.white,
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                    hintText: 'Enter Bottom Text',
+                    hintStyle: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        .copyWith(color: Colors.grey),
+                    focusedBorder: border,
+                    enabledBorder: border.copyWith(
+                        borderSide: BorderSide(color: Colors.grey[600]))),
+                style: Theme.of(context).textTheme.headline6,
+                onChanged: (val) => setState(() => t2.text = val),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Text Style",
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
                 children: [
-                  Image.network(
-                    'https://bingoland.pl/userdata/public/gfx/2652/tlo-fotograficzne-biale.jpg',
-                    width: double.infinity,
-                    fit: BoxFit.fill,
-                    height: 300,
+                  SizedBox(
+                    width: 10,
                   ),
-                  Align(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                      child: AutoSizeText(
-                        t1.text,
-                        textAlign: TextAlign.center,
-                        maxLines: 3,
-                        style: TextStyle(color: Colors.grey[900], fontSize: 36),
-                      ),
-                    ),
-                    alignment: Alignment.topCenter,
+                  _getFontWeightButton("Bold",(){}),
+                  SizedBox(
+                    width: 20,
                   ),
-                  Align(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                      child: AutoSizeText(
-                        t2.text,
-                        textAlign: TextAlign.center,
-                        maxLines: 3,
-                        style: TextStyle(color: Colors.grey[900], fontSize: 36),
-                      ),
-                    ),
-                    alignment: Alignment.bottomCenter,
-                  )
+                  _getFontWeightButton("Normal",(){}),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  _getFontWeightButton("Light",(){}),
+                  SizedBox(
+                    width: 10,
+                  ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            TextFormField(
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                  hintText: 'Enter Top Text',
-                  hintStyle: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(color: Colors.grey),
-                  focusedBorder: border,
-                  enabledBorder: border.copyWith(
-                      borderSide: BorderSide(color: Colors.grey[600]))),
-              style: Theme.of(context).textTheme.headline6,
-              onChanged: (val) => setState(() => t1.text = val),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              cursorColor: Colors.white,
-              decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-                  hintText: 'Enter Bottom Text',
-                  hintStyle: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(color: Colors.grey),
-                  focusedBorder: border,
-                  enabledBorder: border.copyWith(
-                      borderSide: BorderSide(color: Colors.grey[600]))),
-              style: Theme.of(context).textTheme.headline6,
-              onChanged: (val) => setState(() => t2.text = val),
-            ),
-            Spacer(),
-            Container(
-              width: double.infinity,
-              child: FlatButton(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: Text(
-                  "Create Meme",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline3
-                      .copyWith(color: Theme.of(context).primaryColor),
+              Container(
+                width: double.infinity,
+                child: FlatButton(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Text(
+                    "Create Meme",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        .copyWith(color: Theme.of(context).primaryColor),
+                  ),
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  color: Colors.white,
                 ),
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                color: Colors.white,
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
+
+
+  Widget _getFontWeightButton(String title, Function handler) => Expanded(
+    child: FlatButton(
+      padding: EdgeInsets.symmetric(vertical: 15),
+      child: AutoSizeText(
+        title,
+        maxLines: 1,
+        maxFontSize: 18,
+        style: Theme.of(context)
+            .textTheme
+            .headline3
+            .copyWith(color: Theme.of(context).primaryColor),
+      ),
+      onPressed: handler,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)),
+      color: Colors.white,
+    ),
+  );
 }
